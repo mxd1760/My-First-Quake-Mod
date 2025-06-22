b69e3910b57a9172745473d7fbea9a10597b0ce8
Jason Yundt <jason@jasonyundt.email>
2025-06-22 07:08:23 -0400

    Add formatter for flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,12 @@
 {
-  outputs = { self }: {
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  outputs =
+    { nixpkgs, self }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+    {
+      formatter."${system}" = pkgs.nixfmt-rfc-style;
+    };
 }
